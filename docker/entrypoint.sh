9dfc08aa1e8cde0ed27229f1a9b3fce6e12a490c
paludga <gabriele.paludetto@rse-web.it>
2022-11-28 17:14:40 +0100

    test entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+echo "get code from git repo"
+cd $1
+if [ -d .git ]; then
+  echo .git;
+  echo "git already present"
+else
+  echo "init git with remote"
+  git init
+  git remote add origin "${GIT_REMOTE}"
+  git rev-parse --git-dir 2> /dev/null;
+fi;
+
+git reset --hard
+git clean -fd
+git pull origin master
+
+echo "Exec Node app"
+# tail -f /dev/null
 node $1/app.js
